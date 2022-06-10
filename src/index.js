f459a21eedd567b18460c8a5100bc703a581e217
Rodrigo Aizpuru <rodrigoaiz@gmail.com>
2022-06-09 21:58:56 -0500

    scss

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-//import 'bootstrap';
+import 'bootstrap';
 import './sass/styles.scss';
 import 'normalize.css/normalize.css';
 //import './js/menu.js';
-//import 'bootstrap/scss/bootstrap.scss';
+import 'bootstrap/scss/bootstrap.scss';
 import 'animate.css/animate.css';
 import Josh from 'joshjs';
 const josh = new Josh();
