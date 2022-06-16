51eb777c0e4e03e3cb5c1589176064b177a1f26b
Rodrigo Aizpuru <rodrigoaiz@gmail.com>
2022-06-16 16:14:17 -0500

    trabajando seccion de works

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import 'bootstrap';
+import 'bootstrap/scss/bootstrap.scss';
 import './sass/styles.scss';
 import './js/menu.js';
-import 'normalize.css/normalize.css';
+//import 'normalize.css/normalize.css';
 import './js/cerrarredes.js';
 import './js/fondoabout.js';
 import './js/modals.js';
-import 'bootstrap/scss/bootstrap.scss';
 import 'animate.css/animate.css';
 import Josh from 'joshjs';
 const josh = new Josh();
